bb63f1ff7160c6e0490ace8c54ef0398498d0df7
将也ka <940495614@qq.com>
2021-07-05 17:23:20 +0800

    完成用户列表

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Message, Button, Form, FormItem, Input } from 'element-ui'
+import { Message, Switch, Tooltip, Pagination, Table, TableColumn, Button, Card, Row, Col, Form, FormItem, Input, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Dialog, MessageBox } from 'element-ui'
 
-Vue.use(Button, Form, FormItem, Input)
+Vue.use(Button, MessageBox, Dialog, Form, Pagination, Switch, Table, Tooltip, TableColumn, FormItem, Card, Row, Col, Input, Container, Header, Aside, Breadcrumb, BreadcrumbItem, Main, Menu, Submenu, MenuItem, MenuItemGroup)
 
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox
--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,30 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
-
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 Vue.use(VueRouter)
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    { path: '/home', component: Home }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [{ path: '/welcome', component: Welcome },
+            { path: '/users', component: Users }]
+        }
+
+    ]
 })
 // 路由导航守卫
 router.beforeEach((to, from, next) => {
-  if (to.path === '/login') return next()
-  const tokenStr = window.sessionStorage.getItem('token')
-  if (!tokenStr) return next('/login')
-  next()
+    if (to.path === '/login') return next()
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
 })
 export default router
