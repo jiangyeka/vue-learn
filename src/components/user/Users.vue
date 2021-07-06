47e0f71bbc5c3fb0c6df3f3154c1c7356e780c4f
将也ka <940495614@qq.com>
2021-07-06 10:02:56 +0800

    权限开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -70,6 +70,7 @@
                 size="mini"
                 type="warning"
                 icon="el-icon-setting"
+                @click="setRole(scope.row)"
               ></el-button>
             </el-tooltip>
           </template>
@@ -143,6 +144,33 @@
         <el-button type="primary" @click="editUserInfo">确 定</el-button>
       </span>
     </el-dialog>
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close="setRoleDialogClosed"
+    >
+      <div>
+        <p>当前的用户：{{ userInfo.username }}</p>
+        <p>当前的角色：{{ userInfo.role_name }}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectedRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in rolesList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -199,7 +227,11 @@ export default {
         mobile: [{ required: true, message: '请输入手机', trigger: 'blur' },
         { validator: checkMobile, trigger: 'blur' },
         { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }]
-      }
+      },
+      setRoleDialogVisible: false,
+      userInfo: {},
+      rolesList: [],
+      selectedRoleId: ''
     }
   },
   created () {
@@ -287,6 +319,26 @@ export default {
       if (res.meta.status !== 200) { return this.$message.error('删除用户失败！') }
       this.$message.success('删除用户成功！')
       this.getUserList()
+    },
+    async setRole (userInfo) {
+      this.userInfo = userInfo
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) this.$message.error('请求角色列表失败')
+      this.rolesList = res.data
+
+      this.setRoleDialogVisible = true
+    },
+    async saveRoleInfo () {
+      if (!this.selectedRoleId) { return this.$message.error('请选择要分配的角色！') }
+      const { data: res } = await this.$http.put(`users/${this.userInfo.id}/role`, { rid: this.selectedRoleId })
+      if (res.meta.status !== 200) return this.$message.error('更新角色失败')
+      this.$message.success('更新角色成功')
+      this.getUserList()
+      this.setRoleDialogVisible = false
+    },
+    setRoleDialogClosed () {
+      this.selectedRoleId = ''
+      this.userInfo = {}
     }
   }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import { Message, Switch, Tooltip, Pagination, Table, TableColumn, Button, Card, Row, Col, Form, FormItem, Input, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Dialog, MessageBox } from 'element-ui'
+import { Message, Switch, Tooltip, Pagination, Table, TableColumn, Button, Card, Row, Col, Form, FormItem, Input, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Dialog, MessageBox, Tag, Tree, Select, Option } from 'element-ui'
 
-Vue.use(Button, MessageBox, Dialog, Form, Pagination, Switch, Table, Tooltip, TableColumn, FormItem, Card, Row, Col, Input, Container, Header, Aside, Breadcrumb, BreadcrumbItem, Main, Menu, Submenu, MenuItem, MenuItemGroup)
+Vue.use(Button, Tag, MessageBox, Option, Tree, Dialog, Select, Form, Pagination, Switch, Table, Tooltip, TableColumn, FormItem, Card, Row, Col, Input, Container, Header, Aside, Breadcrumb, BreadcrumbItem, Main, Menu, Submenu, MenuItem, MenuItemGroup)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox
