4e0f77112b868d094281910187403f4d81587380
将也ka <940495614@qq.com>
2021-07-06 19:14:07 +0800

    分类功能

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
     console.log(config)
@@ -15,6 +16,7 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.use(ElementUI)
+Vue.component('tree-table', TreeTable)
 new Vue({
     router,
     render: h => h(App)
