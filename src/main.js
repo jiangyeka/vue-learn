20f3805937c37c7e24778ba7bb3d4d8aac1e0e2c
将也ka <940495614@qq.com>
2021-07-09 14:04:30 +0800

    商品功能

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import ElementUI from 'element-ui'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
     console.log(config)
@@ -16,7 +21,19 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.use(ElementUI)
+Vue.use(VueQuillEditor)
+
 Vue.component('tree-table', TreeTable)
+Vue.filter('dateFormat', function (originVal) {
+    const dt = new Date(originVal)
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+    return `${y}-${m}-${d}-${hh}-${mm}-${ss}`
+})
 new Vue({
     router,
     render: h => h(App)
