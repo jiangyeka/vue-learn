cb79948a4e7cf4a15511336e0ac96b4f07a1ccd6
jiangyeka <940495614@qq.com>
2022-07-05 00:04:17 +0800

    Revert "feat -g 初始化项目页面结构"
    This reverts commit 6259278f55d20699125a5e2d666c86e223cc0b3a.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router'
-import ElementPlus from 'element-plus'
-import 
-const app = createApp(App)
-app.use(router).use(ElementPlus).mount('#app')
 
-
-
-// console.log('import.meta.env=', import.meta.env);
+createApp(App).mount('#app')
+console.log('import.meta.env=', import.meta.env);
