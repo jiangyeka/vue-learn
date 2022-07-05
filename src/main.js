9d641328be05d332c1ade0e97dbece8ab2640710
jiangyeka <940495614@qq.com>
2022-07-05 08:39:14 +0800

    feat -g 全局路由配置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+// import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css';
+const app = createApp(App)
+app
+    // .use(router)
+    .use(ElementPlus).mount('#app')
 console.log('import.meta.env=', import.meta.env);
