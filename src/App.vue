7cc50419cbdaee543050982d0d725fb9b3dfcc6e
jiangyeka <940495614@qq.com>
2022-07-05 22:33:14 +0800

    feat -g init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script setup>
-
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
-
 <template>
-  <hello-world />
+  <img  alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
