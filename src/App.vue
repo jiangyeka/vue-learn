a87e99a514e60b437782f0f70c2b727952606244
jiangyeka <940495614@qq.com>
2022-07-06 21:21:58 +0800

    feat -m 测试contribution

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Father from './components/Father.vue';
 </script>
 
 <template>
-  <Father>
-    <template v-slot:header> header</template>
-    <template v-slot:main> main</template>
-    <template v-slot:footer> footer</template>
-    <template v-slot="slotProps">{{
-        slotProps.obj.name
-    }}</template>
-  </Father>
+
 </template>
 
 <style>
