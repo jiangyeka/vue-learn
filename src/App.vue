7677bcf587cf9a320fe327277f10722c0393c4bc
jiangyeka <940495614@qq.com>
2022-07-06 21:51:22 +0800

    feat -l 关于ref和computed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script lang="ts">
+import { computed, ref } from "vue"
+
+export default {
+  name: 'App',
+  setup() {
+    const count = ref(0)
+    const doubleCount = computed(() => {
+      return count.value * 2
+    })
+    const increaseOne = () => {
+      count.value++
+    }
+    return {
+      count,
+      doubleCount,
+      increaseOne
+    }
+  }
+}
 </script>
 
 <template>
-
+  <h1>{{ count }}</h1>
+  <h1>{{ doubleCount }}</h1>
+  <button @click="increaseOne">+++</button>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
