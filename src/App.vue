d830903147a533fa525d4f489acfce62b4000c15
jiangyeka <940495614@qq.com>
2022-07-06 21:19:15 +0800

    feat -g 开发ColumnList组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img  alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import { testData } from './data'
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
