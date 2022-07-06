b20e3cbe522cf7221f6be4c489c34a31ba5f9812
jiangyeka <940495614@qq.com>
2022-07-07 00:58:53 +0800

    feat -m 自定义hook

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script lang="ts">
 import { computed, onMounted, onRenderTriggered, onUpdated, reactive, ref, toRefs, watch, onUnmounted } from "vue"
 import useGetMouseAxis from './hooks/useGetMouseAxis'
+import useUrlLoader from './hooks/useUrlLoader'
 export default {
   name: 'App',
   setup() {
     const { x, y } = useGetMouseAxis()
+    const { loading, error, loaded, result } = useUrlLoader('https://dog.ceo/api/breeds/image/random')
     return {
       x,
       y,
+      loading,
+      error,
+      loaded,
+      result
+
     }
   }
 }
@@ -16,6 +23,10 @@ export default {
 <template>
   <h1>x:{{ x }}</h1>
   <h1>y:{{ y }}</h1>
+  <h1>{{ loaded }}</h1>
+  <h1>{{ loading }}</h1>
+  <h1>{{ error }}</h1>
+  <img :src="result.message" alt="">
 </template>
 
 <style>
---
+++ src/hooks/useUrlLoader.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { ref } from "vue";
+
+function useUrlLoader(url: string) {
+    const result = ref(null)
+    const loading = ref(true)
+    const loaded = ref(false)
+    const error = ref(null)
+    axios.get(url).then((res) => {
+        loading.value = false
+        result.value = res.data
+        loaded.value = true
+    }).catch((error) => {
+        result.value = error
+        loading.value = false
+        error.value = error
+    })
+    return {
+        loading,
+        loaded,
+        error,
+        result
+    }
+}
+export default useUrlLoader
