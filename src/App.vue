c6f1f1b19435634c201a949e2966f828fb78ad6c
jiangyeka <940495614@qq.com>
2022-07-07 00:12:25 +0800

    feat -m 自定义hooks

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,21 @@
 <script lang="ts">
-import { computed, onMounted, onRenderTriggered, onUpdated, reactive, ref, toRefs, watch } from "vue"
-interface DataProps {
-  count: number;
-  doubleCount: number;
-  increaseOne: () => void
-}
+import { computed, onMounted, onRenderTriggered, onUpdated, reactive, ref, toRefs, watch, onUnmounted } from "vue"
+import useGetMouseAxis from './hooks/useGetMouseAxis'
 export default {
   name: 'App',
   setup() {
-    onMounted(() => {
-      console.log('mounted');
-    })
-    onUpdated(() => {
-      console.log('updated');
-
-    })
-    onRenderTriggered((event) => {
-      console.log(event);
-
-    })
-    const clickCountData: DataProps = reactive({
-      count: 0,
-      increaseOne: () => { clickCountData.count++ },
-      doubleCount: computed(() => clickCountData.count * 2)
-    })
-    const refClickCountData = toRefs(clickCountData)
-    const welcomeStr = ref('')
-    const updateWelcomeStr = () => {
-      welcomeStr.value += 'Hello'
-    }
-    watch([welcomeStr, () => clickCountData.count], (o, n) => {
-      document.title = welcomeStr.value
-    })
+    const { x, y } = useGetMouseAxis()
     return {
-      ...refClickCountData,
-      welcomeStr,
-      updateWelcomeStr
+      x,
+      y,
     }
-
   }
 }
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
-  <h1>{{ doubleCount }}</h1>
-  <h1>{{ welcomeStr }}</h1>
-  <button @click="increaseOne">+++</button>
-  <button @click="updateWelcomeStr">updateTitle</button>
+  <h1>x:{{ x }}</h1>
+  <h1>y:{{ y }}</h1>
 </template>
 
 <style>
