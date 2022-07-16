261520b357ff66524ce9c4011092d82338ff350e
jiangyeka <940495614@qq.com>
2022-07-16 23:17:28 +0800

    feat -g new-project

---
+++ vite-app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const msg: string = '<h1>1111</h1>'
+const msgStyle={
+  color:'green',
+  fontSize:'20px'
+}
+const flag:Boolean=false
+
+</script>
+
+<template>
+  <div :class="[flag&&'msg-color']" v-html="msg">
+
+  </div>
+</template>
+
+<style scoped>
+.msg-color{
+  color: aqua;
+}
+</style>
