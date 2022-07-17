3225da9534227597f49cfb1cabe0bf78c9d8ea03
jiangyeka <940495614@qq.com>
2022-07-17 13:38:14 +0800

    feat -g router-init

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import {ref,Ref} from 'vue'
 const msg: string = '<h1>1111</h1>'
 const msgStyle={
   color:'green',
   fontSize:'20px'
 }
 const flag:Boolean=false
-
+const age=ref<string|number>('18')
 </script>
 
 <template>
