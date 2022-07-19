9ef04945202ea04e8dde9d3eb940594865759f6e
jiangyeka <940495614@qq.com>
2022-07-19 22:23:56 +0800

    feat -g pinia

--- vite-app/src/main.ts
+++ vite-app/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import {createPinia} from 'pinia'
+const store=createPinia()
+let app = createApp(App)
+app.use(store).mount('#app')
