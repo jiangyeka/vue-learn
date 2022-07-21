6d1b562e3056c362db4b72245a0e76ee98802b07
jiangyeka <940495614@qq.com>
2022-07-21 21:58:47 +0800

    feat -g init nest项目

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import {useTestStore} from'./store/index'
-const Test=useTestStore()
-const change=()=>{
-  Test.changeName()
-}
 
 </script>
 
 <template>
  <div>
-{{Test.current}}
-{{Test.name}}
-<button @click="change">change</button>
+
+
  </div>
 </template>
 
